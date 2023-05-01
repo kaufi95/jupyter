a6239a7cd683612d8c71088414c80adf489a3b4a
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2023-05-01 15:37:41 +0200

    updated notebooks and directories

--- config/nbgrader_config.py
+++
@@ -1,7 +0,0 @@
-c = get_config()
-c.Exchange.root = "/opt/exchange"
-c.CourseDirectory.course_id = "example_course"
-c.ClearSolutions.code_stub = {
-    "c": "// your code here",
-    "C++11": "// your code here",
-}
