198c200adfa0896ffd86a1b9031f527aa24ca1f8
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2023-05-02 23:11:33 +0200

    readded jupyterlab; nbgrader in hub still to do

--- docker/lab/nbgrader_config.py
+++ docker/lab/nbgrader_config.py
@@ -1,6 +1,6 @@
 c = get_config()
-c.Exchange.root = "/home/jovyan/exchange"
-c.CourseDirectory.course_id = "example_course"
+c.Exchange.root = "/home/jovyan/work/exchange"
+c.CourseDirectory.course_id = "Structured_Programming"
 c.ClearSolutions.code_stub = {
     "c": "// your code here",
     "C++11": "// your code here",
---
+++ docker/singleuser/nbgrader_config.py
@@ -0,0 +1,7 @@
+c = get_config()
+c.Exchange.root = "/home/jovyan/work/exchange"
+c.CourseDirectory.course_id = "Structured_Programming"
+c.ClearSolutions.code_stub = {
+    "c": "// your code here",
+    "C++11": "// your code here",
+}
